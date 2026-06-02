20d624df385de0e4c688381cdbcbda806f7347b1
cesarpoclin@gmail.com <cesarchpo2@gmail.com>
2026-06-02 05:32:11 -0500

    barra lateral completa

--- app/src/main/java/com/example/appgastape/CategoriasActivity.kt
+++ app/src/main/java/com/example/appgastape/CategoriasActivity.kt
@@ -1,17 +1,55 @@
 package com.example.appgastape
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 
 class CategoriasActivity : AppCompatActivity() {
+
+    private lateinit var dlMenu : DrawerLayout
+    private lateinit var ivMenu : ImageView
+    private lateinit var nvMenu : NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_categorias)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        dlMenu = findViewById<DrawerLayout>(R.id.dlMenu)
+        ivMenu = findViewById<ImageView>(R.id.ivMenu)
+        nvMenu = findViewById<NavigationView>(R.id.nvMenu)
+
+        ivMenu.setOnClickListener {
+            dlMenu.open()
+        }
+
+        nvMenu.setNavigationItemSelectedListener { menuItem ->
+
+            when (menuItem.itemId) {
+
+                R.id.itCategoria -> {
+
+                    val intent = Intent(this, CategoriasActivity::class.java)
+                    startActivity(intent)
+
+                }
+                R.id.itInicio -> {
+
+                    val intent =    Intent(this, InicioActivity::class.java)
+                    startActivity(intent)
+                }
+
+            }
+            true
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dlMenu)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/appgastape/EstadisticasActivity.kt
+++ app/src/main/java/com/example/appgastape/EstadisticasActivity.kt
@@ -1,17 +1,53 @@
 package com.example.appgastape
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class EstadisticasActivity : AppCompatActivity() {
+    private lateinit var dlMenu : DrawerLayout
+    private lateinit var ivMenu : ImageView
+    private lateinit var nvMenu : NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estadisticas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        dlMenu = findViewById<DrawerLayout>(R.id.dlMenu)
+        ivMenu = findViewById<ImageView>(R.id.ivMenu)
+        nvMenu = findViewById<NavigationView>(R.id.nvMenu)
+
+        ivMenu.setOnClickListener {
+            dlMenu.open()
+        }
+
+        nvMenu.setNavigationItemSelectedListener { menuItem ->
+
+            when (menuItem.itemId) {
+
+                R.id.itCategoria -> {
+
+                    val intent = Intent(this, CategoriasActivity::class.java)
+                    startActivity(intent)
+
+                }
+                R.id.itEstadistica -> {
+
+                    val intent =    Intent(this, com.example.appgastape.EstadisticasActivity::class.java)
+                    startActivity(intent)
+                }
+
+            }
+            true
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dlMenu)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
